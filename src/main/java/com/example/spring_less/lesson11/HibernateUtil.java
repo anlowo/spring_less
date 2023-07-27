312c5a07697dee77051a845440e8b9aa11cc9092
anlowo <argensarbagysev7@gmail.com>
2023-07-27 19:36:06 +0600

    lol

--- src/main/java/com/example/spring_less/lesson11/HibernateUtil.java
+++ src/main/java/com/example/spring_less/lesson11/HibernateUtil.java
@@ -1,7 +1,27 @@
 package com.example.spring_less.lesson11;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class HibernateUtil {
-    private static SessionFactory factory;
+    private static SessionFactory sessionFactory; // настройка и работа с сессиями (фабрика сессий)
+
+    static {
+        final StandardServiceRegistry registry =
+                new StandardServiceRegistryBuilder() // получение реестра сервисов
+                        .configure().build();
+        try {
+            // MetadataSources - для работы с метаданными маппинга объектов
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        } catch (Exception e) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 }
+
